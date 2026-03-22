d7ba7ef7e54f9ab88ee7fb20e1214ff696abf762
Sankalp Singh <sankalpsinghcoder@gmail.com>
2026-03-22 13:54:22 +0530

    Integrate Express server for bot status
    Added an Express server to respond with a status message.

--- index.js
+++ index.js
@@ -270,3 +270,14 @@ setInterval(() => {
 
 // ================= LOGIN =================
 client.login(process.env.TOKEN);
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
